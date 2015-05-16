9e9dcc685fd768faf978f37eccb6a77db2011a17
pleitgeb <patrick.leitgeb@tshinny.com>
2015-05-16 21:48:29 +0200

    added first draft of mailchimp integration

--- index.php
+++ index.php
@@ -132,6 +132,31 @@ $_SESSION['captcha'] = simple_php_captcha();
                     </h2>  
                     <div class="row top-buffer"></div>
                     <div id="form" >
+                        <!-- Begin MailChimp Signup Form-->
+                        <!--<div id="mc_embed_signup">-->
+                            <form role="form" action="//tshinny.us10.list-manage.com/subscribe/post?u=f8782c91f8c1295341793d3af&amp;id=f73efc1f02" method="POST" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="contact" target="_blank" novalidate>
+                                <div id="col-md-6 column pull-right">
+                                    <div class="form-group">
+                                        <input type="email" value="" name="EMAIL" class="form-control" placeholder="Email address" id="mce-EMAIL" width="50" >
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="input" value="" name="FNAME" class="form-control" placeholder="First name" id="mce-FNAME">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="input" value="" name="LNAME" class="form-control" placeholder="Last name" id="mce-LNAME">
+                                    </div>
+                                    <div id="mce-responses" class="clear">
+                                        <div class="response" id="mce-error-response" style="display:none"></div>
+                                        <div class="response" id="mce-success-response" style="display:none"></div>
+                                    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                    <div style="position: absolute; left: -5000px;"><input type="text" name="b_f8782c91f8c1295341793d3af_f73efc1f02" tabindex="-1" value=""></div>
+                                    <div class="clear"><input type="submit" value="Keep me posted" name="subscribe" id="mc-embedded-subscribe" class="btn btn-default"></div>
+                                </div>
+                            </form>
+                        <!--</div>-->
+                        <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+                        <!--End mc_embed_signup-->
+                        <!--
                         <form role="form" method="POST" class="contact" id="form" >
                             <div class="col-md-6 column pull-right">
                                 <div class="row post-error"></div>
@@ -160,6 +185,7 @@ $_SESSION['captcha'] = simple_php_captcha();
                             </div>
                            
                         </form>
+                        -->
                     </div>
                 </div>
                 <div class="col-md-4 column">
