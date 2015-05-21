c95cfe4d989a7810ed85d0ed1e6eb05679372bd5
Patrick Leitgeb <patrick.leitgeb@gmail.com>
2015-05-21 18:50:37 +0200

    fixed navigation

--- index.php
+++ index.php
@@ -46,17 +46,17 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.php">tshinny.com</a>
+                <a class="navbar-brand" href="#">tshinny.com</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li>
-                        <a href="index.php#team">Team</a>
+                        <a href="#team">Team</a>
                     </li>
 
                     <li>
-                        <a href="index.php#contact">Contact</a>
+                        <a href="#contact">Contact</a>
                     </li>
                 </ul>
             </div>
